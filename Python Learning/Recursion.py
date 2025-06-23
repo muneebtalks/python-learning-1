7f60ce0776fbf36be7c684dac9d1ee0fe8aff420
Muhammad Muneeb <muhammadmuneeb622003@gmail.com>
2025-06-23 18:59:07 +0500

    add new files

--- Python Learning/Recursion.py
+++ Python Learning/Recursion.py
@@ -1,7 +1,5 @@
 # A function calling itself is called recursion.
 # It must have a base case to stop, otherwise it loops forever.
-from operator import truediv
-
 
 # Factorial Example:
 def factorial(n):
@@ -39,7 +37,28 @@ def sum_n(n):
     # Base case: if n is 1, the sum is 1
     if n == 1:
         return 1
-    # Recursive step: sum is n plus the sum of numbers up to n-1
+    # Recursive step: a sum is n plus the sum of numbers up to n-1
     else:
         return n + sum_n(n - 1)
 print(sum_n(9))
+
+
+#Lambda Functions (Anonymous Functions)
+# Normal function
+def square(x):
+    return x * x
+
+# Lambda function
+square = lambda x: x * x
+print(square(5))
+
+#Lambda Useful with map(), filter(), and reduce()
+nums = [1, 2, 3, 4, 5]
+
+# Double each number
+doubled = list(map(lambda x: x * 2, nums))
+print(doubled)  # [2, 4, 6, 8, 10]
+
+# Filter even numbers
+even = list(filter(lambda x: x % 2 == 0, nums))
+print(even)  # [2, 4]
